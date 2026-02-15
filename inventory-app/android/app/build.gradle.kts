42afb072910355881fd2f4f4113452c634daa4a3
Vakiloc <vakiloc@gmail.com>
2026-02-15 14:56:33 +0100

    Revert "Update Retrofit to 3.0.0"
    This reverts commit 72f54d6ed3703a45b2bb77a3dafd8dd334851a29.

--- inventory-app/android/app/build.gradle.kts
+++ inventory-app/android/app/build.gradle.kts
@@ -121,8 +121,8 @@ dependencies {
   // Networking
   implementation("com.squareup.okhttp3:okhttp:4.12.0")
   implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
-  implementation("com.squareup.retrofit2:retrofit:3.0.0")
-  implementation("com.squareup.retrofit2:converter-gson:3.0.0")
+  implementation("com.squareup.retrofit2:retrofit:2.11.0")
+  implementation("com.squareup.retrofit2:converter-gson:2.11.0")
 
   // WebAuthn / Passkeys
   implementation("androidx.credentials:credentials:1.2.2")
